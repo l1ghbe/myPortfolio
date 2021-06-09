81fe631ef0a0a3d5927138e0dc2dd19b6f764bb2
l1ghbe <ee1zet25@gmail.com>
2021-06-10 01:24:30 +0300

    upd loader

--- src/js/script.js
+++ src/js/script.js
@@ -274,7 +274,7 @@ window.addEventListener('load', () => {
     setTimeout(() => {
         loader.classList.add('hidden')
 
-    }, 400)
+    }, 4000)
     setTimeout(() => {
         loader.remove()
     }, 8000)
